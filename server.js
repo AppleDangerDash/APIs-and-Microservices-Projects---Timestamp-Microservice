32851a0f80515868c97d88cca09f03d2373746ac
Mike <haithemharaga@yahoo.co.uk>
2020-09-18 22:44:25 +0100

    done challange 1

--- server.js
+++ server.js
@@ -19,9 +19,33 @@ app.get("/", function (req, res) {
 });
 
 
+app.get("/api/timestamp", (req,res) => {
+  const date = new Date();
+  res.json({
+    unix: date.getTime() , 
+    utc: date.toUTCString()
+  })
+});
+
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/timestamp/:date_str", function (req, res) {
+  const { date_str } = req.params;
+
+  let date = new Date(date_str);
+
+  if(date.toString() === 'Invalid Date'){
+    date = new Date(parseInt(date_str));    
+  } 
+
+  if(date.toString() ==='Invalid Date') {
+    return res.json({"error" : "Invalid Date" });
+  }
+  else{
+    return res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+       })
+  }
 });
 
 
